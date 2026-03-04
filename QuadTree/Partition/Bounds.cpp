9d5602c5c637be8b305b140c9495a67e2ced887a
SuYeon <includeashe@gmail.com>
2026-03-04 16:04:24 +0900

    Feat: Add QuadTree Project(Start)
    [Add]
    - class Bounds: 쿼드 트리에서 겹침을 판정해줄 클래스.

---
+++ QuadTree/Partition/Bounds.cpp
@@ -0,0 +1,33 @@
+#include "Bounds.h"
+
+bool Bounds::Intersects(const Bounds& other) const
+{
+	// 겹치지 않은 경우 먼저 판단.
+
+	// 다른 영역이 오른쪽으로 벗어난 경우.
+	if (other.x < MaxX())
+	{
+		return false;
+	}
+
+	// 다른 영역이 왼쪽으로 벗어난 경우.
+	if (other.MaxX() < x)
+	{
+		return false;
+	}
+
+	// 다른 영역이 아래로 벗어난 경우.
+	if (other.y < MaxY())
+	{
+		return false;
+	}
+
+	// 다른 영역이 위로 벗어난 경우.
+	if (other.MaxY() < y)
+	{
+		return false;
+	}
+
+	// 위에서 걸리지 않음 => 겹침
+	return true;
+}
---
+++ QuadTree/Partition/Bounds.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// 사각형 영역을 표시하는 클래스.
+class Bounds
+{
+public:
+	Bounds(int x, int y, int width = 1, int height = 1)
+		: x(x), y(y), width(width), height(height)
+	{
+	}
+
+	// Getter.
+	inline int X() const { return x; }
+	inline int Y() const { return y; }
+	inline int Width() const { return width; }
+	inline int Height() const { return height; }
+
+	int MaxX() const { return x + width; }
+	int MaxY() const { return y + height; }
+
+	// 겹침 판정 함수
+	// AABB.
+	bool Intersects(const Bounds& other) const;
+
+private:
+	// 왼쪽 상단 지점을 기준으로 위치 표시.
+	int x = 0;
+	int y = 0;
+
+	// 너비/높이.
+	// 상용 Engine은 float 좌표를 쓰지만, Console 좌표는 int를 쓰므로 int로 설정.
+	int width = 1;
+	int height = 1;
+
+};
+
